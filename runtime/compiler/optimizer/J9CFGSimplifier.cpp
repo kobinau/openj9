c9549cee049f785ce7eb1dc12b8d8f06d702c943
Kobi Nauenberg <kobinau@gmail.com>
2019-08-16 15:18:39 -0400

    commented out CFGS env code

--- runtime/compiler/optimizer/J9CFGSimplifier.cpp
+++ runtime/compiler/optimizer/J9CFGSimplifier.cpp
@@ -34,9 +34,9 @@
 
 bool J9::CFGSimplifier::simplifyIfPatterns(bool needToDuplicateTree)
    {
-   static char *enableCFGSimplification = feGetEnv("TR_enableCFGSimplificaiton");
-   if (enableCFGSimplification == NULL)
-      return false;
+   //static char *enableCFGSimplification = feGetEnv("TR_enableCFGSimplificaiton");
+   //if (enableCFGSimplification == NULL)
+    //  return false;
 
    return OMR::CFGSimplifier::simplifyIfPatterns(needToDuplicateTree)
           || simplifyResolvedRequireNonNull(needToDuplicateTree)
